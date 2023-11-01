37e67b8be1449a3cc7b0293755c8d63f3180653c
Mathieu GARNIER <mgarnier11@gmail.com>
2023-11-01 14:29:03 +0100

    feat: added utils

---
+++ libs/utils/src/index.ts
@@ -0,0 +1,7 @@
+import HwCpu from "./types/cpu";
+import HwGpu from "./types/gpu";
+import HwNetwork from "./types/network";
+import HwRam from "./types/ram";
+import HwStorage from "./types/storage";
+
+export { HwCpu, HwGpu, HwRam, HwNetwork, HwStorage };
---
+++ libs/utils/src/simpleCache.ts
@@ -0,0 +1,31 @@
+export class SimpleCache<T> {
+  private data: Map<string, { timestamp: number; value: T }> = new Map();
+  private ttl: number; // Time-to-live in milliseconds
+
+  constructor(ttlSeconds: number = 15) {
+    this.ttl = ttlSeconds * 1000; // Convert to milliseconds
+  }
+
+  set(key: string, value: T): void {
+    const timestamp = Date.now();
+    this.data.set(key, { timestamp, value });
+  }
+
+  get(key: string): T | null {
+    const cached = this.data.get(key);
+
+    if (!cached) return null;
+
+    const now = Date.now();
+    if (now - cached.timestamp > this.ttl) {
+      this.data.delete(key);
+      return null;
+    }
+
+    return cached.value;
+  }
+
+  invalidate(key: string): void {
+    this.data.delete(key);
+  }
+}
