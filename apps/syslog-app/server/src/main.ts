3b29fd6f5b699b7f9ffe6a8d736400c8be13b682
Mathieu GARNIER <mgarnier11@gmail.com>
2024-01-30 19:28:01 +0100

    fix: syslog-app

--- apps/syslog-app/server/src/main.ts
+++ apps/syslog-app/server/src/main.ts
@@ -9,6 +9,7 @@ import { log } from './utils';
 
 const main = async () => {
   console.log('\x1b[36m%s\x1b[0m', 'I am cyan'); //cyan
+  console.log('\x1b[33m%s\x1b[0m', 'I am yellow'); //yellow
 
   log('Starting syslog server');
 
