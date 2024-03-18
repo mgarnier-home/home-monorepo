4a3d0c4bcdc5e5b44ff2a8226e4c5d6783ba992b
Mathieu GARNIER <mgarnier11@gmail.com>
2024-03-18 20:57:11 +0000

    fix: autosaver: docker build

--- build-local.sh
+++ build-local.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
 PROJECT_NAME=${1}
+BUILDX=${2: false}
+PUSH=${3: false}
 
 if [ -z "${PROJECT_NAME}" ]; then
   echo "Usage: $0 <project_name>"
   exit 1
 fi
 
-docker buildx build --platform linux/amd64,linux/arm64 -t mgarnier11/$PROJECT_NAME:latest -f apps/$PROJECT_NAME/docker/Dockerfile . --push
+if [ "${BUILDX}" = true ]; then
+  echo "Building for multiple platforms"
+  docker buildx create --name mybuilder
+  docker buildx use mybuilder
+  docker buildx build --platform linux/amd64,linux/arm64 -t mgarnier11/$PROJECT_NAME:latest -f apps/$PROJECT_NAME/docker/Dockerfile .
+else
+  echo "Building for single platform"
+  docker build -t mgarnier11/$PROJECT_NAME:latest -f apps/$PROJECT_NAME/docker/Dockerfile .
+fi
+
+if [ "${PUSH}" = true ]; then
+  echo "Pushing to Docker Hub"
+  docker push mgarnier11/$PROJECT_NAME:latest
+fi
+
