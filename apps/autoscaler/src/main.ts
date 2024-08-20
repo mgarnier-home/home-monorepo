b0f8e515ead930ab2ac40fcf17d8fda0d5068b7b
Mathieu GARNIER <mgarnier11@gmail.com>
2024-08-20 08:42:42 +0000

    fix: autoscaler: improve dev mode handling

--- apps/autoscaler/src/main.ts
+++ apps/autoscaler/src/main.ts
@@ -1,9 +1,3 @@
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
-// app.listen(3000, () => {
-//   console.log('App listening on port 3000');
-// });
 import EventSource from 'eventsource';
 import express from 'express';
 
@@ -15,20 +9,9 @@ import { config } from './utils/config';
 
 logger.setAppName('autoscaler');
 
-// TODO : add a secret via env variable
 const webhooks = new Webhooks({
   secret: config.webhookSecret,
 });
-
-// webhooks.on('workflow_run', (event) => {
-//   console.log('===============================');
-//   console.log('workflow run event');
-//   console.log('workflow.name : ', event.payload.workflow?.name);
-//   console.log('workflow_run.name : ', event.payload.workflow_run.name);
-//   console.log('workflow_run.status : ', event.payload.workflow_run.status);
-//   console.log('workflow_run.conclusion : ', event.payload.workflow_run.conclusion);
-//   console.log('===============================');
-// });
 
 webhooks.on('workflow_job', (event) => {
   logger.debug('===============================');
@@ -51,40 +34,12 @@ webhooks.on('workflow_job', (event) => {
   }
 });
 
-if (config.nodeEnv === 'production') {
-  logger.info('Running in production mode');
-  const app = express();
-
-  setVersionEndpoint(app);
-
-  app.use(express.json());
-
-  app.post('/webhooks', async (req, res) => {
-    try {
-      await webhooks.verifyAndReceive({
-        id: req.headers['x-request-id'] as string,
-        name: req.headers['x-github-event'] as any,
-        signature: req.headers['x-hub-signature-256'] as string,
-        payload: JSON.stringify(req.body),
-      });
-
-      logger.debug('Webhook verified and received successfully');
-
-      res.status(200).send('OK');
-    } catch (error) {
-      console.error('Error while verifying and receiving webhook : ', error);
-      res.status(401).send('Unauthorized');
-    }
-  });
-
-  app.listen(config.serverPort, () => {
-    logger.info(`App listening on port ${config.serverPort}`);
-  });
-} else {
+if (config.nodeEnv === 'development') {
   logger.info('Running in development mode');
 
   const source = new EventSource(config.smeeUrl);
   source.onmessage = (event: any) => {
+    logger.debug('Received event from smee');
     const webhookEvent = JSON.parse(event.data);
 
     webhooks
@@ -97,3 +52,35 @@ if (config.nodeEnv === 'production') {
       .catch(console.error);
   };
 }
+
+const app = express();
+
+setVersionEndpoint(app);
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).send('OK');
+});
+
+app.post('/webhooks', async (req, res) => {
+  try {
+    await webhooks.verifyAndReceive({
+      id: req.headers['x-request-id'] as string,
+      name: req.headers['x-github-event'] as any,
+      signature: req.headers['x-hub-signature-256'] as string,
+      payload: JSON.stringify(req.body),
+    });
+
+    logger.debug('Webhook verified and received successfully');
+
+    res.status(200).send('OK');
+  } catch (error) {
+    console.error('Error while verifying and receiving webhook : ', error);
+    res.status(401).send('Unauthorized');
+  }
+});
+
+app.listen(config.serverPort, () => {
+  logger.info(`App listening on port ${config.serverPort}`);
+});
