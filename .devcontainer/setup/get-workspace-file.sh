95325cfe1ad2f116ad1a628d784e9d2b9b1997d1
Mathieu GARNIER <mgarnier11@gmail.com>
2025-01-02 22:43:02 +0000

    feat: devcontainer: added /mnt/dev to workspace file

--- .devcontainer/setup/get-workspace-file.sh
+++ .devcontainer/setup/get-workspace-file.sh
@@ -8,18 +8,19 @@ echo $baseWorkspace
 # Get the folders from the base workspace file
 folders=$(jq '.folders // []' <<<"$baseWorkspace")
 
-# For each directory in /mnt/dev, add it to the workspace file
-for dir in /mnt/dev/*; do
-  if [ -d $dir ]; then
-    # Get the base name of the directory
-    basename=$(basename $dir)
+# # For each directory in /mnt/dev, add it to the workspace file
+# Commented for now because im trying to show /mnt/dev folder in the workspace
+# for dir in /mnt/dev/*; do
+#   if [ -d $dir ]; then
+#     # Get the base name of the directory
+#     basename=$(basename $dir)
 
-    echo "Adding $basename to workspace file"
+#     echo "Adding $basename to workspace file"
 
-    # Add the directory to the folders array
-    folders=$(jq --arg name "$basename" --arg path "$dir" '. + [{name: $name, path: $path}]' <<<"$folders")
-  fi
-done
+#     # Add the directory to the folders array
+#     folders=$(jq --arg name "$basename" --arg path "$dir" '. + [{name: $name, path: $path}]' <<<"$folders")
+#   fi
+# done
 
 # Create a new JSON object with the folders array
 newJson=$(jq -n --argjson folders "$folders" '{folders: $folders}')
