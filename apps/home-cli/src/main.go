f0db5b462205c101a3ff7ccf9f54ae5ef2bccb1a
Mathieu GARNIER <mgarnier11@gmail.com>
2025-05-06 09:02:12 +0000

    fix: home-cli: permission issue

--- apps/home-cli/src/main.go
+++ apps/home-cli/src/main.go
@@ -1,8 +1,16 @@
 package main
 
-import "mgarnier11/home-cli/cmd"
+import (
+	"mgarnier11/home-cli/cmd"
+	"os/exec"
+)
 
 func main() {
+	err := exec.Command("chmod", "600", "/run/secrets/ssh_private_key").Run()
+
+	if err != nil {
+		panic(err)
+	}
 
 	cmd.Execute()
 }
