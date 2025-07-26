ecc0999836b731a04b15c5bd5e9e1af88af593ee
mgarnier11 <mgarnier11@gmail.com>
2025-07-26 15:20:40 +0000

    fix: orchestrator: dir of ssh key, home container: API_ORCHESTRATOR_URL

--- .devcontainer/01-container-config.zsh
+++ .devcontainer/01-container-config.zsh
@@ -2,9 +2,9 @@
 
 
 mkdir -p /home/mgarnier/.local
-curl -o "/home/mgarnier/.local/orchestrator-cli" "$ORCHESTRATOR_API_URL/cli?os=linux&arch=amd64"
+curl -o "/home/mgarnier/.local/orchestrator-cli" "$API_ORCHESTRATOR_URL/cli?os=linux&arch=amd64"
 chmod +x /home/mgarnier/.local/orchestrator-cli
 
-echo "export ORCHESTRATOR_API_URL=$ORCHESTRATOR_API_URL" >>~/.zshrc
+echo "export API_ORCHESTRATOR_URL=$API_ORCHESTRATOR_URL" >>~/.zshrc
 echo "source <(orchestrator-cli completion zsh)" >>~/.zshrc
 echo "source <(task --completion zsh)" >>~/.zshrc
